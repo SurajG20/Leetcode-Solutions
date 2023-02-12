a9088d1eafaaad69dbbcae2167e57a8fe0f344f6
SurajG20 <121960253+SurajG20@users.noreply.github.com>
2023-02-12 20:48:29 +0530

    Time: 308 ms (94.94%), Space: 19.4 MB (93.68%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -13,7 +13,7 @@ class Solution:
             if not node:
                 return None
             inorder(node.left)
-            arr.append(node.val)
+            arr.append(node)
             inorder(node.right)
         
         arr = [] # Inorder Traversal
@@ -24,13 +24,40 @@ class Solution:
             
             if left > right:
                 return None
+            
             mid = (left + right)//2
-            node = TreeNode(arr[mid])
+            node = arr[mid]
             node.left = dfs(left,mid-1)
             node.right = dfs(mid+1,right)
             
             return node
         
         return dfs(0,len(arr)-1)
+        
+#         # We do a inorder traversal to get the sorted array.
+#         def inorder(node):
+#             nonlocal arr
+#             if not node:
+#                 return None
+#             inorder(node.left)
+#             arr.append(node.val)
+#             inorder(node.right)
+        
+#         arr = [] # Inorder Traversal
+        
+#         inorder(root)
+    
+#         def dfs(left,right): # We convert this sorted array to a binary search Tree
+            
+#             if left > right:
+#                 return None
+#             mid = (left + right)//2
+#             node = TreeNode(arr[mid])
+#             node.left = dfs(left,mid-1)
+#             node.right = dfs(mid+1,right)
+            
+#             return node
+        
+#         return dfs(0,len(arr)-1)
 
                 
