d9e6a7238506ece0abc7e23a74795fbc0bf54b96
SurajG20 <121960253+SurajG20@users.noreply.github.com>
2023-02-17 21:29:26 +0530

    Time: 39 ms (35.31%), Space: 13.7 MB (98.72%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -5,25 +5,49 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
-        
-        inorder = [] # a list for inorder
-        
-        def dfs(node): # Inorder Traversal
-            nonlocal inorder
-            if not node:
-                return None
-            dfs(node.left)
-            inorder.append(node.val)
-            dfs(node.right)
-            
-        dfs(root)
+        pre = -float('inf') # previous node
+        res =  float('inf') # current result
 
-        res = float(inf) # res
+        def minDiffInBST(self, root):
+            if root is None:
+                return
+
+            self.minDiffInBST(root.left) # search in left half
+            # evaluate and set the current node as the node previously evaluated
+            
+            self.res = min(self.res, root.val - self.pre)
+            
+            self.pre = root.val # keep changing the previous value 
+
+            self.minDiffInBST(root.right) # search in right half
+            
+            return self.res
+
         
-        for i in range(len(inorder)):
-            for j in range(i+1,len(inorder)):
-                res = min(res,inorder[j]-inorder[i])
+        
+        
+        
+        
+        
+        
+        
+#         inorder = [] # a list for inorder
+        
+#         def dfs(node): # Inorder Traversal
+#             nonlocal inorder
+#             if not node:
+#                 return None
+#             dfs(node.left)
+#             inorder.append(node.val)
+#             dfs(node.right)
+            
+#         dfs(root)
+
+#         res = float(inf) # res
+        
+#         for i in range(len(inorder)):
+#             for j in range(i+1,len(inorder)):
+#                 res = min(res,inorder[j]-inorder[i])
                 
-        return res
+#         return res
                 
