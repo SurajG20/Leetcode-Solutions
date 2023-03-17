061eb3d1a2e79849e515e9142d72e238e72467f5
SurajG20 <121960253+SurajG20@users.noreply.github.com>
2023-03-17 10:59:48 +0530

    Time: 164 ms (85.34%), Space: 15.5 MB (53.18%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,9 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i,val in enumerate(nums):
-            if val == 0:
-                nums.remove(val)
-                nums.append(val)
+        # We initialize two pointers i = 0, j = 0. Iterate j over range(len(nums)), and if nums[j] != 0, we swap nums[i] and nums[j], and increment i by 1. It is easy to see the loop invariant that nums[:i+1] contains all nonzero elements in nums[:j+1] with relative order preserved. Hence when j reaches len(nums)-1, nums[:i+1] contains all nonzero elements in nums with relative order preserved.
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != 0:
+                nums[i],nums[j] = nums[j],nums[i]
+                i += 1
         return nums
+#         for i,val in enumerate(nums):
+#             if val == 0:
+#                 nums.remove(val)
+#                 nums.append(val)
+#         return nums
             
