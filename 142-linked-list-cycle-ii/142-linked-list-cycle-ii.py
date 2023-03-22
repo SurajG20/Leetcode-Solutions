914f88fbadd70f795bbf230c29fb07109d342ae3
SurajG20 <121960253+SurajG20@users.noreply.github.com>
2023-03-22 22:24:39 +0530

    Time: 53 ms (59.94%), Space: 17.9 MB (20.08%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -6,25 +6,36 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # both pointer start from head
-        slow, fast = head, head
+        curr = head
+        lookup = set()
         
-        # till fast is not None
-        while fast and fast.next:
+        while curr:
+            if curr in lookup:
+                return curr
+            lookup.add(curr)
+            curr = curr.next
+        return None
+#         # both pointer start from head
+#         slow, fast = head, head
+        
+#         # till fast is not None
+#         while fast and fast.next:
             
-            # slow pointer moves one step, while fast pointer move two step
-            slow = slow.next
-            fast = fast.next.next
+#             # slow pointer moves one step, while fast pointer move two step
+#             slow = slow.next
+#             fast = fast.next.next
             
-            # if they become equal means there is cycle 
-            if slow == fast:
+#             # if they become equal means there is cycle 
+#             if slow == fast:
                 
-                # we reset slow pointer
-                slow = head
+#                 # we reset slow pointer
+#                 slow = head
                 
-                # kept fast as it is, untill they do not collab
-                # there is mathematical reason behind it 
-                while slow != fast:
-                    slow = slow.next
-                    fast = fast.next
-                return slow
+#                 # kept fast as it is, untill they do not collab
+#                 # there is mathematical reason behind it 
+#                 while slow != fast:
+#                     slow = slow.next
+#                     fast = fast.next
+#                 return slow
+
+
